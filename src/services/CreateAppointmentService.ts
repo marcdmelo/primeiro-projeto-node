e77db323ed48152d752ac4a99af204392b102996
Marcos de Melo <marcdmelo@gmail.com>
2021-01-23 10:31:32 -0300

    primeiro projeto node terminado

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,46 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * recebimento das informações
+ * tratativa de erros/excessões
+ * Acesso ao repoitório
+ */
+
+interface Request {
+  provider:string;
+  date: Date;
+}
+
+/**
+ * Dependecy Inversion
+ */
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository ){
+      this.appointmentsRepository = appointmentsRepository;
+  }
+    public execute({ date, provider }:Request):Appointment{
+      const appointmentDate = startOfHour(date);
+
+      const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+          appointmentDate,
+        );
+
+      if(findAppointmentInSameDate){
+        throw Error('This appointment is already booked');
+      }
+
+
+      const appointment = this.appointmentsRepository.create({
+        provider,
+        date: appointmentDate
+      });
+  return appointment
+    }
+}
+
+export default CreateAppointmentService;
